06f0e500d02efd053b980c244924d6c15d7875a6
bgsanders <bobby.sanders.js@braums.com>
2018-04-30 16:09:34 -0500

    added emperor scene from empire strikes back

---
+++ empire.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+vader="trinoids"
+emperor="ralph"
+
+say -v $vader "What is thy bidding\, my master\?"
+say -v $emperor "There is a great disturbance in the Force\."
+say -v $vader "I have felt it\."
+say -v $emperor "We have a new enemy\, the young Rebel who destroyed the Death Star\. I have no doubt this boy is the offspring of Anakin Skywalker\."
+say -v $vader "How is that possible\?"
+say -v $emperor "Search your feelings\, Lord Vader\. You will know it to be true\. He could destroy us\."
+say -v $vader "He\'s just a boy\. Obi\-Wan can no longer help him\."
+say -v $emperor "The Force is strong with him\. The son of Skywalker must not become a Jedi\."       
+say -v $vader "If he could be turned\, he will become a powerful ally\." 
+say -v $emperor "Yes\. He would be a great asset\. Can it be done\?"
+say -v $vader "He will join us or die\, my master\."
